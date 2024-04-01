5f1716df78533fe39e5e330e1995d8a7a8a3e2ab
Jayasurya6720 <jayasurya6720@gmail.com>
2024-04-01 17:25:07 +0530

    cv update

--- src/components/header/headerButton.js
+++ src/components/header/headerButton.js
@@ -1,10 +1,10 @@
 import React from 'react';
-// import res from '../../assets/Jayasurya.pdf'
+import res from '../../assets/Jayasurya CV2.pdf'
 
 const headerButton = () => {
     return (
         <div className='button'>
-            {/* <a href={res} download className='btn'>Download CV </a> */}
+            <a href={res} download className='btn'>Download CV </a>
             <a href='#contact ' className='btn btn-primary'>Let's Talk</a>
         </div>
     )
